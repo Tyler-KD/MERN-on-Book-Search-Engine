9776f31a59873189e7062f79101e9c7348160654
unknown <tyler.kd.knapp@gmail.com>
2024-04-20 19:31:50 -0500

    Created an Apollo Provider to make every request work in the Apollo server

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,6 +1,8 @@
 import { gql } from '@apollo/client';
 
-// Authenticates a user
+// Authenticates a user.
+// email and password as input parameters.
+// If the login is successful, it returns a token and the user's details (ID and username).
 export const LOGIN_USER = gql`
 mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
@@ -13,23 +15,27 @@ mutation login($email: String!, $password: String!) {
 }
 `;
 
-// Create a new user
+// Create a new user.
+// username, email, and password as input parameters.
+// If the user is successfully created, it returns a token and the new user's details (ID and username).
 export const ADD_USER = gql`
-mutation addUser($username: String!, $email: String!, $password: String!) {
+mutation AddUser($username: String!, $email: String!, $password: String!) {
     addUser(username: $username, email: $email, password: $password) {
-        token
-        user {
-            _id
-            username
-        }
+      token
+      user {
+        _id
+        username
+      }
     }
-}
+  }
 `;
 
-// Add a book to a user's saved books
+// Add a book to a user's saved books.
+// BookInput as input parameter.
+// If the book is successfully saved, it returns the user's details (username, email, bookCount) and the list of saved books.
 export const SAVE_BOOK = gql`
-mutation saveBook($book: SavedBookInput!) {
-    saveBook(book: $book) {
+mutation saveBook($book: BookInput!) {
+    saveBook(input: $input) {
         username
         email
         bookCount
@@ -45,9 +51,11 @@ mutation saveBook($book: SavedBookInput!) {
 }
 `;
 
-// Remove a book from a user's saved books
+// Remove a book from a user's saved books.
+// bookId as input parameter.
+// If the book is successfully removed, it returns the user's details (username, email, bookCount) and the updated list of saved books.
 export const REMOVE_BOOK = gql`
-mutation removeBook($bookId: String!) {
+mutation removeBook($bookId: ID!) {
     removeBook(bookId: $bookId) {
         username
         email
